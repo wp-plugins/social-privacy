7b0bf26095f7f322b306e05ec4ab28b412fef7bc
multinc <multinc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-11 10:57:53 +0000

    - Added plugin URI plus description to warn users if this top-level directory was improperly installed in the wp-content/plugins/ directory
    git-svn-id: https://plugins.svn.wordpress.org/social-privacy/trunk@64250 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ WARNING-each-subdirectory-must-be-installed-separately.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Social Privacy
+Plugin URI: http://multinc.com/wp/social-privacy/
+Description: *** INSTALLATION ERROR *** You've made an installation mistake.  Social Privacy is a set of plugins.  So the subdirectories inside 'social-privacy/' each needs to be installed directly into 'wp-content/plugins/'
+Version: 1.1
+Author: Multinc
+Author URI: http://multinc.com/
+*/
+?>
